cf8a3cd6da79bf3406b8f5d6dd6c7eea459b6fe1
kumaramar35 <43752742+kumaramar35@users.noreply.github.com>
2026-02-20 07:03:59 +0530

    Update CORS to remove localhost origins
    Removed localhost origins from CORS configuration.

--- server.js
+++ server.js
@@ -284,8 +284,6 @@ process.on('uncaughtException', (err) => console.error('Uncaught Exception:', er
 
 app.use(cors({
    origin: [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
   "https://premvatika.com",
   "https://www.premvatika.com"
 ],
